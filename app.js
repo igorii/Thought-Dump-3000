26adc51a1dbf07c5616b18889b12b80885a6e659
Tim Thornton <t.thornton6@gmail.com>
2013-12-06 02:14:53 +0000

    Solarized insired style

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // All environments
 app.set('view engine', 'ejs');
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
