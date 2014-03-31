b2ddc4922c365164b62d7f63e75d40aa49d5ac68
Tim Thornton <t.thornton6@gmail.com>
2014-03-31 17:21:42 +0000

    Simplifying gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,8 @@
 var gulp   = require('gulp');
-var tinylr = require('tiny-lr')();
 var app    = require('./app');
-var path   = require('path');
-
-function handleFileChange (e) {
-    var file = path.relative('public', e.path);
-
-    console.log('');
-    console.log('Changed ' + file);
-    tinylr.changed({
-        body: {
-            files: [file]
-        }
-    });
-}
 
 gulp.task('default', function () {
-    gulp.run('lr-server');
     gulp.run('main-server');
-
-    gulp.watch('public/stylesheets/*.css', handleFileChange);
-});
-
-gulp.task('lr-server', function () {
-    tinylr.listen(35729);
 });
 
 gulp.task('main-server', function () {
